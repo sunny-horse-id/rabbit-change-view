d04dbcf9e53f52c2c7bfe418035cd096c240d735
zy <1824762582@qq.com>
2024-05-28 11:28:23 +0800

    登录的统一

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -23,7 +23,7 @@ const onGetphonenumber: UniHelper.ButtonOnGetphonenumber = async (ev) => {
 
 // 模拟手机号码快捷登录（开发练习）
 const onGetphonenumberSimple = async () => {
-  const res = await postLoginWxMinSimpleAPI('13123456789')
+  const res = await postLoginWxMinSimpleAPI('19189970856')
   loginSuccess(res.result)
 }
 
